c34b7737fbd8d8cf54de63ec1ca3177733fc12d6
RodriJOk <juarezrodrigo59gmail.com>
2021-12-19 12:19:47 -0300

    Modify components in the project

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -6,7 +6,7 @@ const Order = () => {
 		<div className="Order">
 			<p>
 				<span>03.25.21</span>
-				<span>6 articles</span>
+				<span>7 articles</span>
 			</p>
 			<p>$560.00</p>
 			<img src="./icons/flechita.svg" alt="arrow" />
